d71f3dd2578d215fa8f0ec20ca675c99f375226f
wahlen <wahlen@iutlogin.iut.icepp.s.u-tokyo.ac.jp>
2024-06-10 21:51:11 +0900

    faster importation

--- load_awkward.py
+++ load_awkward.py
@@ -2,7 +2,7 @@ import json
 import h5py
 import awkward as ak
 import numpy as np
-
+from time import time
 def load_awkward2(filename):
     file = h5py.File(filename) #Create File object                                                                                                                                   
     feat = file["feature"] #access the feature group, File contains two subgroups, feature and label                                                                                 
@@ -44,16 +44,28 @@ def load_awkward2(filename):
 def load_awkwards(filenames):
     #print(f"{filenames=}")
     assert(len(filenames)>0)
+    feats_list, labels_list = [], []
     for i, file in enumerate(filenames):
         print(f"Reading file: {file=}")
         feat, label = load_awkward2(file)
-        if i==0:
-            ak_feats = feat
-            ak_labels = label
-        else:
-            ak_feats = ak.concatenate((ak_feats, feat), axis=0)
-            ak_labels = ak.concatenate((ak_labels, label), axis=0)
-
+        feats_list.append(feat)
+        labels_list.append(label)
+    #print(len(feats_list))
+    #print(ak.num(feats_list[0], axis = 0))
+    #print(ak.num(feats_list[0],axis = 1))
+    #start = time()    
+    #feats_ak = ak.flatten(ak.Array(feats_list),axis = 1)
+    #print(time() - start)
+    #labels_ak = ak.flatten(ak.Array(labels_list), axis = 1)
+    #return feats_ak, labels_ak
+        #if i==0:
+        #    ak_feats = feat
+        #    ak_labels = label
+        #else:
+    start = time()
+    ak_feats = ak.concatenate(feats_list, axis=0)
+    ak_labels = ak.concatenate(labels_list, axis=0)
+    print(time()-start)
         #print (ak.num(ak_feats,axis=0), ak.num(ak_labels,axis=0))                                                                                                                   
 
     return ak_feats, ak_labels
