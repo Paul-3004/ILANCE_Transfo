fb9bdf4b41a08fcf5578cad7067ae93e8f8aedf7
Paul-3004 <77359118+Paul-3004@users.noreply.github.com>
2024-06-15 21:38:23 +0900

    iter 1 V2

--- model.py
+++ model.py
@@ -2,21 +2,118 @@ from collections import OrderedDict
 import torch
 import torch.nn as nn
 from torch.nn.functional import softmax
+from torch.nn.modules.transformer import _get_clones
+
 
 class Embedder(nn.Module):
-    def __init__(self, nlayers,d_input ,d_model,act_func = nn.ReLU(), dtype = torch.float32):
+    def __init__(self, nlayers,d_input ,d_model,d_hid,act_func = nn.ReLU(), dtype = torch.float32):
         super(Embedder, self).__init__()
-        linear = nn.Linear(d_input,d_model, dtype = dtype)
-        sequence_module = OrderedDict([("input_layer", linear)])
-        sequence_module.update([("hidden_actfun1", act_func)])
-        for i in range(1, nlayers+1):
-            linear = nn.Linear(d_model,d_model, dtype =  dtype) #otherwise shares same parameters
-            sequence_module.update([("hidden_actfun%d"%i, act_func)])
-            sequence_module.update([("hidden_linear%d"%i, linear)])
-        
+        assert d_hid >  d_model, "Embedder: dimension of hidden layers should be greater than output dimension"
+        if nlayers == 1:
+            sequence_module = OrderedDict([("input_layer", nn.Linear(d_input,d_model, dtype = dtype))])
+            sequence_module.update([("hidden_actfun1", act_func)])
+        elif nlayers > 1:
+            sequence_module = OrderedDict([("input_layer", nn.Linear(d_input,d_hid, dtype = dtype))])
+            sequence_module.update([("hidden_actfun1", act_func)])
+            if nlayers > 2:
+                for i in range(2, nlayers ):
+                    linear = nn.Linear(d_hid,d_hid, dtype =  dtype) #otherwise shares same parameters
+                    sequence_module.update([("hidden_linear%d"%i, linear)])
+                    sequence_module.update([("hidden_actfun%d"%i, act_func)])
+            sequence_module.update([("hidden_linear%d"%nlayers, nn.Linear(d_hid, d_model, dtype = dtype))])
+
         self.model = nn.Sequential(sequence_module)
+
     def forward(self,src):
         return self.model(src)
+
+'''
+Creates the custom decoder layer for V2 of implementation. 
+Input is first processed by nproj projection matrices. Each of those output vector then undergoes 
+classical decoder S-A and MHA. The results are then concatenated and fed to the Feed Forward Network
+Calling the original nn.TransformerDecoderLayer.__init__() will create sequentially:
+    - 1 Self-Attention module 
+    - 1 Multi-Head Attention module 
+    - 1 2 layered Feed Forward network 
+This cusotm decoder layer adds:
+    - nproj Projection matrices for the input
+    - nproj - 1 additional Self-Attention modules
+    - nproj - 1 additional MH Attention modules
+
+    Args:
+        d_model: dimension of input vector (after embedding)
+        nproj: number of projection matrices to be created
+        nhead: number of heads for the attention process
+        dim_ff: dimension of the feed forward network
+'''
+# class CustomDecoderLayer(nn.TransformerDecoderLayer):
+#     def __init__(d_model, nproj, nhead, dim_ff, device, dtype):
+#         super(CustomDecoderLayer,self).__init__(d_model = d_model, nhead=nhead,dim_feedforward=dim_ff,device = device, dtype = dtype)
+
+
+class CustomDecoder(nn.Module):
+    def __init__(self,layer, nlayers):
+        super(CustomDecoder, self).__init__()
+        self.nlayers = nlayers
+        self.layers = _get_clones(layer, nlayers)
+    
+    def forward(self,tgt, memory,tgt_mask, tgt_key_padding_mask, memory_key_padding_mask, tgt_is_causal):
+        output = tgt
+        for layer in self.layers:
+            output = layer(output, memory, tgt_mask = tgt_mask,
+                                          tgt_key_padding_mask = tgt_key_padding_mask,
+                                          memory_key_padding_mask =memory_key_padding_mask, 
+                                          tgt_is_causal = tgt_is_causal)
+        return output
+
+class CustomDecoderLayer(nn.Module):
+    def __init__(self,d_model, d_proj,nproj, nhead, dim_ff_sub, dim_ff_main, batch_first, dtype, device,activation = nn.ReLU(), dropout = 0.1):
+        super(CustomDecoderLayer, self).__init__()
+        self.dtype = dtype
+        self.device = device
+        
+        dim_proj = d_model // nproj
+        assert nproj * dim_proj == d_model, "CustomDecoderLayer: d_model must be divisible by nproj"
+        self.nproj = nproj
+        self.dim_proj = dim_proj
+        
+        projection = nn.Linear(d_model, d_proj,dtype = dtype, device = device, bias = False)
+        decoder_sublayer = nn.TransformerDecoderLayer(d_proj,nhead,dim_ff_sub,batch_first=batch_first, device = device, dtype = dtype)
+
+        self.projections = _get_clones(projection, nproj)
+        self.decoder_layers = _get_clones(decoder_sublayer,nproj)
+
+        #main feed forward
+        self.linear1 = nn.Linear(d_model,dim_ff_main)
+        self.dropout_ff1 = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_ff_main,d_model)
+        self.dropout_ff2 = nn.Dropout(dropout)
+        #Layer Normalization
+        self.norm1 = nn.LayerNorm(d_model,device= device, dtype = dtype)
+        self.norm2 = nn.LayerNorm(d_model,device= device, dtype = dtype)
+
+
+
+    def forward(self, tgt, memory, tgt_mask, tgt_key_padding_mask, memory_key_padding_mask, tgt_is_causal):
+        assert self.dim_proj == memory.shape[-1], "CustomDecoderLayer: Dimension of decoder input after projection and encoder output must match."
+        size_proj = tgt.shape
+        size_proj[-1] = self.dim_proj
+        output = torch.zeros(self.nproj, *size_proj, dtype =  self.dtype, device = self.device)
+        for i in range(self.nproj):
+            proj = self.projections[i](tgt)
+            output[i] = self.decoder_layers[i](proj, memory, tgt_mask = tgt_mask,
+                                                 tgt_key_padding_mask = tgt_key_padding_mask,
+                                                 memory_key_padding_mask = memory_key_padding_mask,
+                                                 tgt_is_causal = tgt_is_causal)
+        output = torch.concatenate(output.tensor_split(self.nproj, dim = 0), dim = -1).squeeze(0)
+        output = self.norm1(output + tgt) # layer normalization
+        output = self.norm2(output + self.main_ff_block(output))
+        return output
+
+    def main_ff_block(self, x):
+        x = self.linear2(self.dropout_ff1(self.linear1(x)))
+        return self.dropout_ff2(x)
+
 
 
 # Implements a neural network composed of an Embedder converting d_input hits to d_model vectors, 
@@ -31,28 +128,37 @@ class Embedder(nn.Module):
 #                  - d_model: shared with Embedder 
 #                  - for the rest, see docs at https://pytorch.org/docs/stable/generated/torch.nn.Transformer.html            
 class ClustersFinder(nn.Module):
-    def __init__(self, dmodel, nhead, nhid_ff_trsf, nlayers_encoder, device,
-                                                                nlayers_decoder,nlayers_embder, 
+    def __init__(self, dmodel, nhead, d_ff_trsf, device, nlayers_encoder,
+                                                                nlayers_decoder,
+                                                                custom_decoder = None,
+                                                                nlayers_embedder_src: int = 2, 
+                                                                d_ff_embedder_src: int = 256,
+                                                                d_out_embedder_src: int = 128,
+                                                                nlayers_embedder_tgt: int = 2,
+                                                                d_ff_embedder_tgt: int = 512,
+                                                                d_out_embedder_tgt: int = 256,
                                                                 d_input_encoder: int = 6,
                                                                 d_input_decoder: int = 8,
                                                                 nparticles_max: int = 10, 
                                                                 ncharges_max: int = 3,
                                                                 DOF_continous: int = 3,
-                 dtype = torch.float32
+                                                                dtype = torch.float32
                                                                 ):
         
         super(ClustersFinder,self).__init__()
         self.device = device
         self.dtype = dtype
-        self.input_embedder = Embedder(nlayers = nlayers_embder, d_input=d_input_encoder,d_model=dmodel, dtype = dtype)
-        self.tgt_embedder = Embedder(nlayers = nlayers_embder, d_input=d_input_decoder,d_model=dmodel, dtype = dtype)
+        self.input_embedder = Embedder(nlayers = nlayers_embedder_src, d_input=d_input_encoder,d_model=dmodel,d_hid = d_ff_embedder_src, dtype = dtype)
+        self.tgt_embedder = Embedder(nlayers = nlayers_embedder_tgt, d_input=d_input_decoder,d_model=dmodel, d_hid = d_ff_embedder_tgt, dtype = dtype)
         self.transformer = nn.Transformer(d_model=dmodel, 
                                           nhead = nhead, 
-                                          dim_feedforward= nhid_ff_trsf,
+                                          dim_feedforward= d_ff_trsf,
                                           num_encoder_layers=nlayers_encoder, 
                                           num_decoder_layers=nlayers_decoder, 
+                                          custom_decoder= custom_decoder,
                                           batch_first= True,
-                                          dtype = dtype)
+                                          dtype = dtype,
+                                          device = device)
 
         self.lastlin_charge = nn.Linear(dmodel,ncharges_max, dtype= dtype)
         self.lastlin_pdg = nn.Linear(dmodel,nparticles_max, dtype = dtype)
