81adabe5aa973bc0d4141f04c6bd24d6d32625d8
Paul-3004 <77359118+Paul-3004@users.noreply.github.com>
2024-06-21 13:02:32 +0900

    preprocessing

---
+++ preprocess.py
@@ -0,0 +1,6 @@
+from data_prepro import train_val_preprocessing
+
+dir_res  = "/Users/paulwahlen/Desktop/Internship/ML/Code/TransfoV1/data/"
+dir_train = "/Users/paulwahlen/Desktop/Internship/ML/Code/TransfoV1/data/"
+
+train_val_preprocessing(dir_train, dir_train, dir_res)
