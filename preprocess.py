d70f38b662693f27bc3e1b872fcaea303a6e99cf
wahlen <wahlen@iutgpu01.iut.icepp.s.u-tokyo.ac.jp>
2024-06-26 15:02:10 +0900

    max memory management

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,24 @@
 from data_prepro import train_val_preprocessing, get_data
+from time import time
+import numpy as np
 
-dir_res  = "/Users/paulwahlen/Desktop/Internship/ML/Code/TransfoV1/data/training/"
-dir_train = "/Users/paulwahlen/Desktop/Internship/ML/Code/TransfoV1/data/training/"
+dir_res  = "/data/suehara/mldata/pfa/ntau_10to100GeV_10/preprocessed"
+dir_train = "/data/suehara/mldata/pfa/ntau_10to100GeV_10/train"
+dir_val =  "/data/suehara/mldata/pfa/ntau_10to100GeV_10/validation"
+frac_files = 0.5
 
-#train_val_preprocessing(dir_train, dir_train, dir_res)
-
-vocab_charges, vocab_pdgs, special_symbols, E_rms_normalizer, train_dl, val_dl = get_data((dir_res, dir_res), 10, 1, "training", True)
-
-for i,(feats, labels) in enumerate(train_dl):
-    print(labels)
-    print(feats)
-    pass
+testing = False
+if testing:
+    start = time()
+    vocab_charges, vocab_pdgs, special_symbols, E_rms_normalizer, train_dl, val_dl = get_data((dir_res + "/training", dir_res + "/validation"), 10, 1, "training", True)
+    dt = time() - start
+    print(f"time to load from preprocessed {dt} sec")
+    for i,(feats, labels) in enumerate(train_dl):
+       # print(labels)
+       # print(feats)
+        pass
+else:
+    fracs = np.arange(6,11)/ 10
+    for frac_files in fracs:
+        train_val_preprocessing(dir_train, dir_val, dir_res, frac_files)
+        
