e316cf6015ce2a96980080ae820b56c5ebfd9d89
Paul-3004 <77359118+Paul-3004@users.noreply.github.com>
2024-07-23 20:37:27 +0900

    get_vocabs.py

---
+++ get_vocabs.py
@@ -0,0 +1,50 @@
+import torch
+import os
+import numpy as np
+from data_prepro import CollectionHits
+
+
+def get_pdg(path):
+    special_symbols = {
+        "pad":0,
+        "bos":1,
+        "eos":2,
+        "sample":3
+    }
+    ds_col = CollectionHits(path,special_symbols,1,False)
+    ds_col.process_dataset()
+    dict_pdg = ds_col.vocab_pdgs.vocab
+    return list(dict_pdg.values())
+
+if __name__=="__main__":
+    dir_ds = "/data/suehara/mldata/pfa/"
+    datasets = ["ntau_10to100GeV_10", "gamma_10to100GeV_1", "gamma_5to50GeV_2", "eg_5to50GeV_1", "el_10to100GeV_1", "el_5to50GeV_2"]
+
+    pdg_all = []
+    _, vocab_ntau_train, _ = torch.load("/data/suehara/mldata/pfa/ntau_10to100GeV_10/preprocessed/training/vocabs/vocabs_normalizer.pt")
+    pdg_ntau_train = list(vocab_ntau_train.values())
+    print(f"pdg ntau train: {pdg_ntau_train}")
+    pdg_all = []
+    pdg_all.append(pdg_ntau_train)
+    for ds in datasets:
+        subdirs = ["train", "validation","test"]
+        ds_with_sub = ["ntau_10to100GeV_10", "gamma_10to100GeV_1", "gamma_5to50GeV_2"]
+        dir_voc = os.path.join(dir_ds,ds)
+        if ds in ds_with_sub:
+            for subdir in subdirs:
+                if not ((ds == "ntau_10to100GeV_10") and (subdir == "train")):
+                    dir_voc = os.path.join(dir_ds,ds,subdir)
+                    print(f"getting pdg of {dir_voc}")
+                    pdg = get_pdg(dir_voc)
+                    print(pdg)
+                    pdg_all.append(pdg)
+
+        else:
+            print(f"getting pdg of {dir_voc}")
+            pdg = get_pdg(dir_voc)
+            pdg_all.append(dir_voc)
+
+pdg_np = np.array(pdg_all)
+pdg_unique = np.unique(pdg_np)
+print(f"unique pdgs: {pdg_unique}")
+torch.save(pdg_unique,"vocab_values_all_ds.pt")
